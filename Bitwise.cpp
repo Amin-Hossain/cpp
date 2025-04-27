9d0f25bae00bfb9661ef55fc1f0d68412697c012
ABIDUL HASAN AMIN <113846592+Amin-Hossain@users.noreply.github.com>
2025-04-27 14:07:28 +0600

    Add files via upload

---
+++ Bitwise.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main() {
+
+int a,b,c;
+a = 32;
+b = 12 ;
+c = a&b; // Bitwise AND
+cout <<c<<endl;
+c = a | b; // Bitwise OR
+cout <<c<<endl;
+c = a^b; // Bitwise XOR
+cout <<c<<endl;
+c = a<<3; //Left
+cout <<c<<endl;
+c = a>>3; //Right
+cout <<c<<endl;
+
+
+
+
+
+}
