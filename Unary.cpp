7f2c0b40346faa869459b8b1b051e0aa1fe5d4f0
ABIDUL HASAN AMIN <113846592+Amin-Hossain@users.noreply.github.com>
2025-04-27 13:42:52 +0600

    Add files via upload

---
+++ Unary.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main (){
+int a = 2;
+int b = +a;
+cout <<b<<endl;
+b = -a;
+cout <<b<<endl;
+b = a++;
+cout<<a<<endl;
+cout <<b<<endl;
+b = ++a;
+cout <<a<<endl;
+cout <<b<<endl;
+b = a--;
+    cout <<a << endl;
+    cout <<b<<endl;
+    b = --a;
+    cout <<a << endl;
+    cout <<b<<endl;
+
+
+
+
+
+
+
+
+
+
+
+}
