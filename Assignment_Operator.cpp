79fe39958c8a940eb249d86da62c5775a739155b
ABIDUL HASAN AMIN <113846592+Amin-Hossain@users.noreply.github.com>
2025-04-27 13:27:15 +0600

    Add files via upload

---
+++ Assignment_Operator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main (){
+
+int x = 2;
+x+=2; //x = x + 2
+cout <<x<<endl;
+x-=2 ;
+cout << x<<endl;
+x*=2;
+cout << x<<endl;
+x/=2;
+cout<<x<<endl;
+x%=2;
+cout <<x<<endl;
+
+
+getch ();
+
+
+
+
+
+
+}
