0cff96547550c7bad2a1c80633009b27d9c736cc
ABIDUL HASAN AMIN <113846592+Amin-Hossain@users.noreply.github.com>
2025-05-07 12:34:56 +0600

    Add files via upload

---
+++ Even_Odd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main (){
+
+int a ;
+cout <<"Enter a number \n"<<endl;
+cin>>a;
+if (a%2==0)
+    {
+
+    cout <<"The number is even"<<endl;
+}
+else {
+    cout<<"The number is odd"<<endl;
+}
+
+getch ();
+
+
+
+
+
+}
