99f3aa7cd2e33ff434463c700ec4be6fb6005405
ABIDUL HASAN AMIN <113846592+Amin-Hossain@users.noreply.github.com>
2025-05-07 13:11:35 +0600

    Add files via upload

---
+++ Vowel_Check.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main (){
+char ch;
+cout <<"Enter a small capital number"<<endl;
+cin>>ch;
+if (ch== 'a' || ch== 'e' || ch== 'i' || ch== 'o' || ch== 'u') {
+        cout<<"The number is Vowel"<<endl;
+
+}
+else {
+    cout<<"The number is Constant"<<endl;
+}
+getch () ;
+
+
+
+}
